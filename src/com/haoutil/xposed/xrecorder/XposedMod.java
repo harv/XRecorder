856c3f74ed836c4e854a6b40ffc8e649ce0a6c38
cinhoo <ch05042210@gmail.com>
2014-06-24 15:13:53 +0800

    saved file as callerName_phoneNumber_yyyyMMddHHmmss.amr format.

--- src/com/haoutil/xposed/xrecorder/XposedMod.java
+++ src/com/haoutil/xposed/xrecorder/XposedMod.java
@@ -4,6 +4,7 @@ import java.lang.reflect.Field;
 
 import com.android.internal.telephony.Call;
 import com.android.internal.telephony.CallManager;
+import com.android.internal.telephony.CallerInfo;
 
 import de.robv.android.xposed.IXposedHookLoadPackage;
 import de.robv.android.xposed.XC_MethodHook;
@@ -13,6 +14,7 @@ import de.robv.android.xposed.callbacks.XC_LoadPackage.LoadPackageParam;
 
 public class XposedMod implements IXposedHookLoadPackage {
 	private Field mApp, mCM, mCallRecorder;
+	private String phoneName, phoneNumber;
 	
 	@Override
 	public void handleLoadPackage(LoadPackageParam lpparam) throws Throwable {
@@ -21,29 +23,52 @@ public class XposedMod implements IXposedHookLoadPackage {
 		}
 		
 		Class<?> clazz1 = XposedHelpers.findClass("com.android.phone.SomcInCallScreen", lpparam.classLoader);
-		Class<?> clazz2 = XposedHelpers.findClass("com.android.phone.PhoneGlobals", lpparam.classLoader);
+		Class<?> clazz2 = XposedHelpers.findClass("com.android.phone.SomcCallRecorder", lpparam.classLoader);
+		Class<?> clazz3 = XposedHelpers.findClass("com.android.phone.PhoneGlobals", lpparam.classLoader);
 		
 		mApp = XposedHelpers.findField(clazz1, "mApp");
 		mCM = XposedHelpers.findField(clazz1, "mCM");
-		mCallRecorder = XposedHelpers.findField(clazz2, "mCallRecorder");
+		mCallRecorder = XposedHelpers.findField(clazz3, "mCallRecorder");
 		
-		hook(clazz1);
+		hook1(clazz1);
+		hook2(clazz2);
 	}
 
-	private void hook(Class<?> clazz) {
+	private void hook1(Class<?> clazz) {
 		XposedBridge.hookAllMethods(clazz, "onPhoneStateChanged", new XC_MethodHook() {
 			@Override
 			protected void afterHookedMethod(MethodHookParam param) throws Throwable {
-				CallManager cm = (CallManager) mCM.get(param.thisObject);
 				Object callRecorder = mCallRecorder.get(mApp.get(param.thisObject));
-				
 				if ((Boolean) XposedHelpers.callMethod(callRecorder, "isRecording")) {
 					return;
 				}
-				
-				if (cm.getActiveFgCallState() == Call.State.ACTIVE) {
+
+				CallManager cm = (CallManager) mCM.get(param.thisObject);
+				Call.State state = cm.getActiveFgCallState();
+				if (state == Call.State.ACTIVE) {
 					XposedHelpers.callMethod(callRecorder, "start");
+				} else if (state == Call.State.ALERTING || state == Call.State.INCOMING || state == Call.State.WAITING) {
+					CallerInfo callerInfo = (CallerInfo) XposedHelpers.callMethod(param.thisObject, "getCallerInfoFromConnection",
+							XposedHelpers.callMethod(param.thisObject, "getConnectionFromCall", cm.getActiveFgCall()));
+					phoneName = callerInfo.name;
+					phoneNumber = callerInfo.phoneNumber.startsWith("sip:") ? callerInfo.phoneNumber.substring(4) : callerInfo.phoneNumber;
 				}
+			}
+		});
+	}
+
+	private void hook2(Class<?> clazz) {
+		XposedBridge.hookAllMethods(clazz, "generateFilename", new XC_MethodHook() {
+			@Override
+			protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+				String result = ((String) param.getResult()).replaceAll("-", "");
+				if (phoneNumber != null && !"".equals(phoneNumber)) {
+					result = phoneNumber + "_" + result;
+				}
+				if (phoneName != null && !"".equals(phoneName)) {
+					result = phoneName + "_" + result;
+				}
+				param.setResult(result);
 			}
 		});
 	}
